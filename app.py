2bcef41d01d570559dc4d81cab6b66988ba5b4c8
Mann-ai <donhero022001@gmail.com>
2025-02-19 11:24:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify
-import random
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    prediction = random.choice(["UP", "DOWN"])  # Future me AI Model laga sakte ho
-    return jsonify({"prediction": prediction})
+@app.route('/')
+def home():
+    return "Hello, Flask is running on Render!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+
